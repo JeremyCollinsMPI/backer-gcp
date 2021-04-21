84fab1fd22c431e1e0b7df12c2e8d84e8af09e45
Jeremy Collins <jeremycollinsgithub2@gmail.com>
2021-04-21 14:08:37 +0800

    changed timeout

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 gcloud builds submit --tag gcr.io/backer-1585550181964/backergcp
-gcloud run deploy demo10 --image gcr.io/backer-1585550181964/backergcp --platform managed --memory 8Gi --cpu 2 --region asia-east1 --allow-unauthenticated
+gcloud run deploy demo10 --image gcr.io/backer-1585550181964/backergcp --platform managed --memory 8Gi --cpu 2 --region asia-east1 --allow-unauthenticated --timeout 3600
