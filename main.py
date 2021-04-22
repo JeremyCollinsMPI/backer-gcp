0be6715d822b1ae0adb6c8fccc7b329a41f8c7f1
Jeremy Collins <jeremycollinsgithub2@gmail.com>
2021-04-22 18:59:26 +0800

    correction

--- main.py
+++ main.py
@@ -2,14 +2,10 @@ import os
 
 from flask import Flask, request
 from transformers import pipeline
+from inference import *
 
 app = Flask(__name__)
 
-classifier = pipeline("zero-shot-classification",
-                      model="facebook/bart-large-mnli")
-                      
-example = classifier('I like to travel', ['travel'])
- 
 @app.route('/status', methods=['GET'])
 def status():
   return {'statusCode': 200}
@@ -21,13 +17,24 @@ def run_path():
   query = content['query']
   threshold = 0.5
   result = []
-  candidate_labels = [query]
   for sentence in sentences:
-    temp = classifier(sentence, candidate_labels)
-    if temp['scores'][0] > threshold:
+    prob = evaluate_inference(sentence, 'This sentence is about' + query)
+    if prob > threshold:
       result.append(sentence)
   return {'result': result}
-
+    
+@app.route('/inference', methods=['POST'])
+def inference_path():
+  content = request.get_json(force=True)
+  sentences = content['sentences']
+  hypothesis = content['hypothesis']
+  threshold = 0.5
+  result = []
+  for sentence in sentences:
+    prob = evaluate_inference(sentence, hypothesis)
+    if prob > threshold:
+      result.append(sentence)
+  return {'result': result}  
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8080)
