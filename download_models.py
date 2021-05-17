8f8b92450e3465f9650ddc0a68173a3a54a8f0be
Jeremy Collins <jeremycollinsgithub2@gmail.com>
2021-05-17 19:41:16 +0800

    attempt to download models in the image

--- download_models.py
+++ download_models.py
@@ -6,3 +6,4 @@ tokenizer = AutoTokenizer.from_pretrained('valhalla/distilbart-mnli-12-1')
 
 qa_tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased-distilled-squad")
 qa_model = AutoModelForQuestionAnswering.from_pretrained("distilbert-base-uncased-distilled-squad")
+
